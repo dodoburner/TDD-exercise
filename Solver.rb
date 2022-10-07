fe1e1daac9d2df1fafdf0934e36957334bd6b126
Owusu-Desmond <desmond0406114009@gmail.com>
2022-10-07 09:23:02 +0000

    Refactor factorial method

--- Solver.rb
+++ Solver.rb
@@ -1,20 +1,7 @@
 class Solver
-  def factorial(num)
-    begin
-      !num.positive?
-    rescue => exception
-      puts exception
+  def factorial(n)  
+    raise ArgumentError, "Only positive integers are allowed" if n < 0
+    return 1 if n == 0
+    (1..n).inject(:*)
     end
-
-    if (num == 0) 
-      1
-    end
-
-    res = 1
-    while num > 1 do
-      res = res * num
-      num = num - 1
-    end
-    res
-  end
 end
